4098ea920c6df6e648aefd65ffedd69a968de780
Deepak Kohli <deepakkohli1810@gmail.com>
2025-05-15 21:09:55 +0530

    Frontend related things has been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { use, useState } from 'react'
+import React, { useState } from 'react'
 import { useGSAP } from '@gsap/react' 
 import gsap from 'gsap'
-
+import 'remixicon/fonts/remixicon.css'
 const App = () => {
    const [showContent , setShowContent] = useState(false);
 
@@ -29,6 +29,13 @@ const App = () => {
       }
     })
   })
+
+  useGSAP(() => {
+    const main = document.querySelector(".main");
+    main?.addEventListener("mousemove", (e) => {
+      console.log(e);
+    } );
+  } ,[showContent]);
 
   return (
    
@@ -77,11 +84,25 @@ const App = () => {
                 </div>
                 <h2 className='text-4xl text-white leading-none -mt-2 '>Rockstar</h2>
               </div>
-      </div>
+                </div>
+             
             <div className='imagediv relative overflow-hidden  w-full h-screen '>
               <img src="./sky.png" alt="sky"  className=' absolute top-0 left-0 w-full h-screen object-cover' />
               <img src='./bg.png' alt='bg' className='w-full absolute h-screen object-cover'/>
+                <div className='text absolute  left-1/2 -translate-x-1/2 '>
+                  <h2 className='text-[11rem] ml-20 text-white font-bold leading-none'>Grand </h2>
+                  <h2 className='text-[11rem] -ml-40 text-white font-bold leading-none'>Theft</h2>
+                  <h2 className='text-[11rem] ml-20 text-white font-bold leading-none'>Auto</h2>
+                </div>
               <img src="./girlbg.png"  alt="" className=' absolute w-[38%] -bottom-[35%]  left-1/2 -translate-x-1/2' />
+               
+            </div>
+            <div className='bottombar text-white absolute bottom-0 left-0  w-full py-15 px-10 bg-gradient-to-t from-black'>
+            <div className='flex gap-4 items-center'>
+            <i className="text-3xl ri-arrow-down-line"></i>
+            <h2 className='text-2xl'> Scroll Down</h2>
+            </div>
+            <img className='h-[55px] absolute top-1/2 left-1/2 -translate-1/2' src="./ps5.png" alt="" />
             </div>
           </div>     
      </div>
