00f6bbf59ca61872e8e786483ebe63543f8c8fae
Deepak Kohli <deepakkohli1810@gmail.com>
2025-05-25 11:28:21 +0530

    Completed the project with necessary animations

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
       duration: 2,
       ease: 'Power4.easeInOut',
       transformOrigin: '50% 50%',
+    
     })
     .to('.vi-mask-group', {
       scale:10,
@@ -31,6 +32,45 @@ const App = () => {
   })
 
   useGSAP(() => {
+    if(!showContent) return; 
+    gsap.to(".main", {
+      scale: 1,
+      rotate: 0,
+      duration: 1,
+      ease: "Power4.easeInOut",
+      
+    })
+     gsap.to(".sky", {
+      scale: 1.1,
+      rotate: 0,
+      duration: 1,
+      ease: "Power4.easeInOut",
+      
+    })
+    gsap.to(".bg", {
+      scale: 1.1,
+      rotate: 0,
+      duration: 1,
+      ease: "Power4.easeInOut",
+      
+    })
+       gsap.to(".character", {
+      scale: 1,
+      bottom: '-45%',
+      rotate: 0,
+      duration: 1.2,
+      
+      ease: "Power4.easeInOut",
+      
+    })
+        gsap.to(".text", {
+      scale: 1,
+      rotate: 0,
+      duration: 1.2,
+      
+      ease: "Power4.easeInOut",
+      
+    })
     const main = document.querySelector(".main");
     main?.addEventListener("mousemove", (e) => {
       const xMove = (e.clientX / window.innerWidth - 0.5) * 40;
@@ -83,9 +123,9 @@ const App = () => {
   </svg>
 </div>
      {showContent && 
-     <div className='main w-full '>
+     <div className='main w-full rotate-[-10deg] scale-[1.7]'>
   
-          <div className='landing w-full h-screen bg-black'>
+          <div className='landing overflow-hidden w-full h-screen bg-black'>
                 <div className='navbar absolute top-0 left-0  z-10  px-10 py-10 w-full '>
               <div className='logo flex items-center gap-7 '>
                 <div className='lines flex flex-col gap-1.5 '>
@@ -99,23 +139,23 @@ const App = () => {
                 </div>
              
             <div className='imagesdiv  relative overflow-hidden  w-full h-screen '>
-              <img src="./sky.png" alt="sky"  className=' sky absolute top-0 left-0 w-full h-screen object-cover' />
-              <img src='./bg.png' alt='bg'  className='bg scale-[2] w-full absolute h-screen object-cover'/>
-                <div className='text flex flex-col gap-4 absolute  left-1/2 -translate-x-1/2 '>
+              <img src="./sky.png" alt="sky"  className=' sky  scale-[1.7] rotate-[-3deg] absolute top-0 left-0 w-[100%] h-screen object-cover' />
+              <img src='./bg.png' alt='bg'  className='bg scale-[1.5] rotate-[-3deg] w-full absolute h-screen object-cover'/>
+                <div className='text flex flex-col gap-4 absolute scale[1.4] rotate-[-20deg]  left-1/2 -translate-x-1/2 '>
                   <h2 className='text-[11rem]  ml-20 text-white font-bold leading-none'>Grand </h2>
                   <h2 className='text-[11rem]  -ml-60 text-white font-bold leading-none'>Theft</h2>
                   <h2 className='text-[11rem]  ml-30 text-white font-bold leading-none'>Auto</h2>
                 </div>
-              <img src="./girlbg.png"  alt="" className=' character absolute w-[38%] -bottom-[35%]  left-1/2 -translate-x-1/2' />
-               
-            </div>
-            <div className='bottombar text-white absolute bottom-0 left-0  w-full py-15 px-10 bg-gradient-to-t from-black'>
+              <img src="./girlbg.png"  alt="" className=' character scale-[1.1] rotate-[-20deg] absolute w-[38%] -bottom-[150%]  left-1/2 -translate-x-1/2' />
+                <div className='bottombar text-white absolute bottom-0 left-0  w-full py-15 px-10 bg-gradient-to-t from-black'>
             <div className='flex gap-4 items-center'>
             <i className="text-3xl ri-arrow-down-line"></i>
             <h2 className='text-2xl'> Scroll Down</h2>
             </div>
             <img className='h-[45px] absolute top-1/2 left-1/2 -translate-1/2' src="./ps5.png" alt="" />
             </div>
+            </div>
+           
           </div>     
      
     <div className='w-full h-screen flex 10px py-10 items-center justify-center bg-black'>
@@ -124,8 +164,14 @@ const App = () => {
          <img 
          className='absolute w-[80%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ' src="./imag.png" alt="" />
         </div>
-        <div className='rg'>
-          <h1>Children are running </h1>
+        <div className='rg w-[40%] max-w-4xl mx-auto p-4'>
+          <h1 className='text-7xl'>Still running </h1>
+          <h1 className='text-7xl'>not hunting  </h1>
+          <p className='mt-4 font-sans'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi nemo illum quibusdam beatae doloribus enim totam, architecto commodi, suscipit assumenda explicabo veritatis sed, sapiente nihil alias asperiores earum. Voluptatem sunt a asperiores eligendi voluptas harum adipisci cumque voluptatum in porro distinctio pariatur, nesciunt ab nobis quidem vel eveniet quas quos.
+          </p>
+          <p className='mt-4 font-sans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, cumque!</p>
+          <button className='bg-yellow-500 px-8 py-3 text-black mt-4 font-sans font-bold '> Download Now </button>
         </div>
       </div>
 
